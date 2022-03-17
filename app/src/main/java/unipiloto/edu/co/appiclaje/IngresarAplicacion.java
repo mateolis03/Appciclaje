1278c819ad7f5d42ce254b4e86859d63d4ebce42
Laura Duarte <lduarteperez2@gmail.com>
2022-03-16 23:13:41 -0500

    validaciones

--- app/src/main/java/unipiloto/edu/co/appiclaje/IngresarAplicacion.java
+++ app/src/main/java/unipiloto/edu/co/appiclaje/IngresarAplicacion.java
@@ -9,10 +9,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -22,42 +28,83 @@ public class IngresarAplicacion extends AppCompatActivity {
     Button boton;
     FirebaseDatabase database;
     DatabaseReference myRef;
-    TextView regUsuario, regPassword;
+    TextView regEmail, regPassword;
     String dbuser, dbpassword;
+    String email;
+    String password;
+    FirebaseAuth  firebaseAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_aplicacion);
+        regEmail = findViewById(R.id.email_address);
+        regPassword = findViewById(R.id.password);
+        firebaseAuth = FirebaseAuth.getInstance();
+        database=  FirebaseDatabase.getInstance();
+        myRef = database.getReference("Users");
+
 
     }
 
     public void registrarUsuario(View view) {
-        Intent intent = new Intent(this, registrarUsuario.class);
+        Intent intent = new Intent(this, registration.class);
         startActivity(intent);
     }
 
     public void home(View view) {
-        regUsuario = findViewById(R.id.user);
-        regPassword = findViewById(R.id.password);
-        Intent intent = new Intent(this, home.class);
-        startActivity(intent);
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users");
-        myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.e("firebase", "Error getting data", task.getException());
-                } else {
-                    dbuser = String.valueOf(task.getResult().getValue());
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                }
-            }
-        });
-        String nickname = regUsuario.getText().toString();
-        String password = regPassword.getText().toString();
+        email = regEmail.getText().toString();
+        password = regPassword.getText().toString();
+        Validation validation = new Validation();
+        boolean pass = true;
+        if (validation.isEmpty(email) || !validation.isEmail(email)) {
+            Toast.makeText(this, "Ingrese su usuario", Toast.LENGTH_LONG).show();
+            pass = false;
+        }
+        if (validation.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese su contraseña", Toast.LENGTH_LONG).show();
+            pass = false;
+        }
+        if (pass){
 
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(IngresarAplicacion.this, "Login correcto", Toast.LENGTH_LONG).show();
+                    String id = firebaseAuth.getCurrentUser().getUid();
+                    DatabaseReference username = myRef.child(id).child("nickname");
+                    username.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String nickname = snapshot.getValue().toString();
+                            Intent intent = new Intent(IngresarAplicacion.this, home.class);
+                            intent.putExtra("nickname", nickname);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+                Toast.makeText(IngresarAplicacion.this, "Error en credenciales", Toast.LENGTH_LONG).show();
+            }
+
+
+    });
     }
+    }
+
+
+    public void recuperarContraseña(View view) {
+        startActivity(new Intent(this, OlvidarContrasena.class));
+        finish();
+    }
+
+
 }
+
---
+++ app/src/main/java/unipiloto/edu/co/appiclaje/registration.java
@@ -0,0 +1,145 @@
+package unipiloto.edu.co.appiclaje;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class registration extends AppCompatActivity {
+    private Button regBtn;
+    private DatabaseReference database;
+    private DatabaseReference myRef;
+    private TextView regName, regEmail, regUser, regPassword, regTelephone, regAddress;
+    private Validation validation;
+    public  FirebaseAuth firebaseAuth;
+    private String name = "";
+    private String email ="";
+    private String nickname ="";
+    private String password = "";
+    private String telephone = "";
+    private String address = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+        regName = findViewById(R.id.nombre);
+        regEmail = findViewById(R.id.email_address);
+        regUser = findViewById(R.id.user);
+        regPassword = findViewById(R.id.password);
+        regTelephone = findViewById(R.id.number);
+        regAddress = findViewById(R.id.address);
+        regBtn = findViewById(R.id.registrarButton);
+    }
+    public void register(View view) {
+        validation = new Validation();
+        boolean cont = true;
+        database = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        name = regName.getText().toString();
+        email = regEmail.getText().toString();
+        nickname = regUser.getText().toString();
+        password = regPassword.getText().toString();
+        telephone = regTelephone.getText().toString();
+        address = regAddress.getText().toString();
+       // Usuario usuario = new Usuario(name, email, password, telephone, address);
+        if (validation.isEmpty(name) || validation.isNumeric(name)) {
+            Toast.makeText(this, "Ingrese un nombre valido", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (validation.isEmpty(email) || !validation.isEmail(email)) {
+            Toast.makeText(this, "Ingrese un correo valido", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (validation.isEmpty(nickname)) {
+            Toast.makeText(this, "Ingrese una contraseña", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (validation.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese una contraseña", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (validation.isTeleTelephone(telephone)) {
+            Toast.makeText(this, "Ingrese una número de telefono valido", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (validation.isEmpty(address)) {
+            Toast.makeText(this, "Ingrese una dirección valida", Toast.LENGTH_LONG).show();
+            cont = false;
+        }
+        if (cont) {
+            registerUser(email,password);
+        }
+    }
+    public void registerUser(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name",name);
+                    map.put("email",email);
+                    map.put("nickname",nickname);
+                    map.put("telephone",telephone);
+                    map.put("address",address);
+
+                    String id = firebaseAuth.getCurrentUser().getUid();
+                    database.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                                if (task2.isSuccessful()){
+                                    Toast.makeText(registration.this, "Usuario registrado", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(registration.this,IngresarAplicacion.class));
+                                    finish();
+                                }
+
+                        }
+                    });
+                }else
+                    Toast.makeText(registration.this, "Este correo ya está registrados", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+             /*   HashMap<String, Usuario> set = new HashMap<>();
+                if (!set.containsKey(nickname)) {
+                    set.put(nickname, usuario);
+                    database = FirebaseDatabase.getInstance();
+                    myRef = database.getReference("users");
+                    myRef.child(nickname).setValue(usuario);
+                    regName.setText("");
+                    regEmail.setText("");
+                    regUser.setText("");
+                    regPassword.setText("");
+                    regTelephone.setText("");
+                    regAddress.setText("");
+                }
+                Toast.makeText(this, "Registro exitoso!",Toast.LENGTH_LONG).show();
+            }*/
+
+    public void back(View view) {
+        Intent intent = new Intent(this, IngresarAplicacion.class);
+        startActivity(intent);
+    }
+
+}
